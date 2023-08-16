cf0c8175ad35e88eddc574fa19d17eda2b2c0a3d
jaskang <jaskang@wesure.cn>
2023-08-16 18:55:00 +0800

    feat: theme config

---
+++ src/theme/extendTheme.ts
@@ -0,0 +1,210 @@
+function breakpoints(screens: Record<string, string>) {
+  return Object.keys(screens)
+    .filter(key => typeof screens[key] === 'string')
+    .reduce(
+      (breakpoints, key) => ({
+        ...breakpoints,
+        [`screen-${key}`]: screens[key],
+      }),
+      {}
+    )
+}
+
+export const extendTheme = (theme: Theme) => {
+  theme.accentColor = {
+    ...theme.colors,
+    auto: 'auto',
+  }
+  theme.backgroundColor = theme.colors
+  theme.borderColor = { ...theme.colors, DEFAULT: theme.colors.gray[200] }
+  theme.boxShadowColor = theme.colors
+  theme.caretColor = theme.colors
+  theme.fill = { ...theme.colors, none: 'none' }
+  theme.gradientColorStops = theme.colors
+  theme.outlineColor = theme.colors
+  theme.placeholderColor = theme.colors
+  theme.ringColor = { ...theme.colors, DEFAULT: theme.colors.blue[500] }
+  theme.ringOffsetColor = theme.colors
+  theme.stroke = { ...theme.colors, none: 'none' }
+  theme.textColor = theme.colors
+  theme.textDecorationColor = theme.colors
+
+  theme.backgroundOpacity = theme.opacity
+  theme.backdropOpacity = theme.opacity
+  theme.borderOpacity = theme.opacity
+  theme.placeholderOpacity = theme.opacity
+  theme.ringOpacity = { ...theme.opacity, DEFAULT: '0.5' }
+  theme.textOpacity = theme.opacity
+
+  theme.backdropBlur = theme.blur
+  theme.backdropBrightness = theme.brightness
+  theme.backdropContrast = theme.contrast
+  theme.backdropGrayscale = theme.grayscale
+  theme.backdropHueRotate = theme.hueRotate
+  theme.backdropInvert = theme.invert
+  theme.backdropSaturate = theme.saturate
+  theme.backdropSepia = theme.sepia
+  theme.divideColor = theme.borderColor
+  theme.divideOpacity = theme.borderOpacity
+  theme.divideWidth = theme.borderWidth
+
+  theme.margin = { ...theme.spacing, auto: 'auto' }
+  theme.borderSpacing = theme.spacing
+  theme.gap = theme.spacing
+  theme.padding = theme.spacing
+  theme.scrollMargin = theme.spacing
+  theme.scrollPadding = theme.spacing
+  theme.space = theme.spacing
+  theme.textIndent = theme.spacing
+  theme.translate = {
+    ...theme.spacing,
+    '1/2': '50%',
+    '1/3': '33.333333%',
+    '2/3': '66.666667%',
+    '1/4': '25%',
+    '2/4': '50%',
+    '3/4': '75%',
+    full: '100%',
+  }
+  theme.inset = {
+    ...theme.spacing,
+    auto: 'auto',
+    '1/2': '50%',
+    '1/3': '33.333333%',
+    '2/3': '66.666667%',
+    '1/4': '25%',
+    '2/4': '50%',
+    '3/4': '75%',
+    full: '100%',
+  }
+  theme.flexBasis = {
+    ...theme.spacing,
+    auto: 'auto',
+    '1/2': '50%',
+    '1/3': '33.333333%',
+    '2/3': '66.666667%',
+    '1/4': '25%',
+    '2/4': '50%',
+    '3/4': '75%',
+    '1/5': '20%',
+    '2/5': '40%',
+    '3/5': '60%',
+    '4/5': '80%',
+    '1/6': '16.666667%',
+    '2/6': '33.333333%',
+    '3/6': '50%',
+    '4/6': '66.666667%',
+    '5/6': '83.333333%',
+    '1/12': '8.333333%',
+    '2/12': '16.666667%',
+    '3/12': '25%',
+    '4/12': '33.333333%',
+    '5/12': '41.666667%',
+    '6/12': '50%',
+    '7/12': '58.333333%',
+    '8/12': '66.666667%',
+    '9/12': '75%',
+    '10/12': '83.333333%',
+    '11/12': '91.666667%',
+    full: '100%',
+  }
+
+  theme.height = {
+    ...theme.spacing,
+    auto: 'auto',
+    '1/2': '50%',
+    '1/3': '33.333333%',
+    '2/3': '66.666667%',
+    '1/4': '25%',
+    '2/4': '50%',
+    '3/4': '75%',
+    '1/5': '20%',
+    '2/5': '40%',
+    '3/5': '60%',
+    '4/5': '80%',
+    '1/6': '16.666667%',
+    '2/6': '33.333333%',
+    '3/6': '50%',
+    '4/6': '66.666667%',
+    '5/6': '83.333333%',
+    full: '100%',
+    screen: '100vh',
+    svh: '100svh',
+    lvh: '100lvh',
+    dvh: '100dvh',
+    min: 'min-content',
+    max: 'max-content',
+    fit: 'fit-content',
+  }
+  theme.width = {
+    ...theme.spacing,
+    auto: 'auto',
+    '1/2': '50%',
+    '1/3': '33.333333%',
+    '2/3': '66.666667%',
+    '1/4': '25%',
+    '2/4': '50%',
+    '3/4': '75%',
+    '1/5': '20%',
+    '2/5': '40%',
+    '3/5': '60%',
+    '4/5': '80%',
+    '1/6': '16.666667%',
+    '2/6': '33.333333%',
+    '3/6': '50%',
+    '4/6': '66.666667%',
+    '5/6': '83.333333%',
+    '1/12': '8.333333%',
+    '2/12': '16.666667%',
+    '3/12': '25%',
+    '4/12': '33.333333%',
+    '5/12': '41.666667%',
+    '6/12': '50%',
+    '7/12': '58.333333%',
+    '8/12': '66.666667%',
+    '9/12': '75%',
+    '10/12': '83.333333%',
+    '11/12': '91.666667%',
+    full: '100%',
+    screen: '100vw',
+    svw: '100svw',
+    lvw: '100lvw',
+    dvw: '100dvw',
+    min: 'min-content',
+    max: 'max-content',
+    fit: 'fit-content',
+  }
+  theme.maxHeight = {
+    ...theme.spacing,
+    none: 'none',
+    full: '100%',
+    screen: '100vh',
+    svh: '100svh',
+    lvh: '100lvh',
+    dvh: '100dvh',
+    min: 'min-content',
+    max: 'max-content',
+    fit: 'fit-content',
+  }
+  theme.maxWidth = {
+    none: 'none',
+    0: '0rem',
+    xs: '20rem',
+    sm: '24rem',
+    md: '28rem',
+    lg: '32rem',
+    xl: '36rem',
+    '2xl': '42rem',
+    '3xl': '48rem',
+    '4xl': '56rem',
+    '5xl': '64rem',
+    '6xl': '72rem',
+    '7xl': '80rem',
+    full: '100%',
+    min: 'min-content',
+    max: 'max-content',
+    fit: 'fit-content',
+    prose: '65ch',
+    ...breakpoints(theme.screens),
+  }
+}
--- theme.md
+++ theme.md
@@ -1,3 +1,163 @@
-能否为 theme 提供函数式来实现配置的继承
+能否为 theme 属性提供函数形式来实现配置的继承。
 
-当一个 preset 中包含大量
+```ts
+// preset -> theme
+type ThemeConfig<T> =  T | ({ extend }: ThemeCtx) => T
+
+const theme: ThemeConfig = {
+  colors,
+  accentColor: ({ extend }) => ({
+    //  支持函数形式
+    ...extend('colors'),
+    auto: 'auto',
+  }),
+}
+```
+
+## Why
+
+当一个 preset 中有大量 theme 需要配置，其中有些配置在默认情况下是互相继承的。这时候使用者需要编写大量重复的配置。
+
+如下例子中：有一个 preset 中有 colors 的配置，而 `accentColor`,`borderColor`,`fill` 都继承自 `colors` 配置。
+
+```ts
+// preset -> theme
+const colors = {
+  black: '#000',
+  white: '#fff',
+  ....
+}
+
+const theme = {
+  colors: colors,
+  accentColor: {
+    ...colors,
+    auto: 'auto',
+  },
+  borderColor:{
+    ...colors,
+    DEFAULT: colors.gray.200,
+  },
+  fill: {
+    ...colors,
+    none: 'none',
+  }
+}
+```
+
+当用户使用该 preset 且需要自定义配置时，需要对每一个 color 相关的配置(`accentColor`,`borderColor`,`fill`) 都写一遍
+
+如：
+
+```ts
+// unocss.config.ts
+export default defineConfig({
+  theme: {
+    colors: {
+      color1: 'color1'
+      color2: 'color2'
+    },
+    accentColor: {
+      color1: 'color1'
+      color2: 'color2',
+      auto: 'auto',
+    },
+    borderColor:{
+      color1: 'color1'
+      color2: 'color2',
+      DEFAULT: colors.gray.200,
+    },
+    fill: {
+      color1: 'color1'
+      color2: 'color2',
+      none: 'none',
+    }
+  },
+  presets: [
+    myPreset(),
+  ],
+})
+```
+
+## Feat
+
+理论上 preset 应该做到: 开发者只需要配置 `colors` 就能做到 `accentColor`,`borderColor`,`fill` 同步更改。同时也可以做到覆盖这些配置。
+
+如：
+
+```ts
+// preset -> theme
+const theme = {
+  colors,
+  accentColor: ({ extend }) => ({
+    //  支持函数形式
+    ...extend('colors'),
+    auto: 'auto',
+  }),
+  borderColor: ({ extend }) => ({
+    //  支持函数形式
+    ...extend('colors'),
+     DEFAULT: colors.gray.200,
+  }),
+}
+```
+
+```ts
+// unocss.config.ts
+const theme = {
+  colors: {
+    color1: 'color1'
+    color2: 'color2'
+  },
+  accentColor: ({ extend }) => ({
+    color1: 'color1'
+    color2: 'color2'
+    DEFAULT: 'transparent',
+  }),
+}
+```
+
+我上述的例子中默认就是使用的 `extendTheme`, `extendTheme` 只是将 theme 配置对象深度合并，还是需要更改每一个配置，并不能满足需求的。
+
+如：
+
+```ts
+// unocss.config.ts
+defineConfig({
+  extendTheme:{
+    colors: {
+      color1: 'color1'
+      color2: 'color2'
+    },
+    accentColor: {
+      color1: 'color1'
+      color2: 'color2',
+      auto: 'auto',
+    },
+    borderColor:{
+      color1: 'color1'
+      color2: 'color2',
+      DEFAULT: colors.gray.200,
+    },
+    fill: {
+      color1: 'color1'
+      color2: 'color2',
+      none: 'none',
+    }
+  }
+})
+```
+
+如果支持函数式的话，只需要更改 `colors` 配置, `accentColor`,`borderColor`, `fill` 这些继承自`colors` 的配置都能生效。
+
+```ts
+// unocss.config.ts
+defineConfig({
+  extendTheme:{
+    colors: {
+      color1: 'color1'
+      color2: 'color2'
+    },
+  }
+})
+```
